b598b287943b47972c029ba5d3b0ef75c53c1a87
paulc <paulc@DESKTOP-JD941BF>
2017-03-01 15:35:12 +0000

    Add project files.

---
+++ src/CQRSExample/Controllers/TodoController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using CQRSExample.Commands.ToDoItem;
+using CQRSExample.Logging;
+using CQRSExample.Models;
+using CQRSExample.Queries.ToDoItem;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRSExample.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly Logger _logger;
+
+        public TodoController(IMediator mediator, Logger logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            var result = _mediator.Send(new GetAll.Query()).Result;
+
+            _logger.Messages.ForEach(m => Debug.WriteLine(m));
+            
+            return result.TodoItems;
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var result = _mediator.Send(new GetById.Query(id)).Result;
+
+            _logger.Messages.ForEach(m => Debug.WriteLine(m));
+
+            return new ObjectResult(result);
+        }
+
+        [HttpPost]
+        public void Add(Add.Command command)
+        {
+            _mediator.Send(command);
+
+            _logger.Messages.ForEach(m => Debug.WriteLine(m));
+        }
+    }
+}
---
+++ src/CQRSExample/Queries/ToDoItem/GetAll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CQRSExample.Models;
+using MediatR;
+
+namespace CQRSExample.Queries.ToDoItem
+{
+    public class GetAll
+    {
+        public class Query : IRequest<Result>
+        {
+        }
+
+        public class Result
+        {
+            public IEnumerable<Models.TodoItem> TodoItems { get; set; }
+        }
+
+        public class QueryHandler : IRequestHandler<Query, Result>
+        {
+            private readonly TodoContext _db;
+
+            public QueryHandler(TodoContext db)
+            {
+                _db = db;
+            }
+
+            public Result Handle(Query message)
+            {
+                var result = new Result { TodoItems = _db.TodoItems.AsEnumerable() };
+
+                return result;
+            }
+        }
+    }
+}
